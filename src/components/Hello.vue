9f827db59269d79bb1532561015d1b4a98dec008
Siddhesh Mangela <sidthecool007@gmail.com>
2020-12-11 10:51:07 +0530

    Helllo msg added

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Vite + Vue 3 + TypeScript + Tailwind 2 Boilerplate</h1>
+  <img src="../assets/logo.png" alt="vue" class="block mx-auto mt-16" />
+  <h1 class="text-center mt-16 text-xl">
+    Vite + Vue 3 + TypeScript + Tailwind 2 Boilerplate
+  </h1>
 </template>
 
 <script lang="ts">
